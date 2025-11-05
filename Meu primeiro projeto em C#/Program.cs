2e953fd6cbd5a19efc0b3782595f32cd28ae4f6f
luizg <luizg@MAQUINA-DE-LUIZ>
2025-11-04 23:39:54 -0300

    Adicionar arquivos de projeto.

---
+++ Meu primeiro projeto em C#/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.WriteLine("Bem vindos ao meu primeiro programa em c#");
+int idade;
+double altura,salario;
+char genero;
+string nome;
+
+Console.WriteLine("Digite seu nome:");
+nome = Console.ReadLine();
+Console.WriteLine("Digite sua idade:");
+    idade = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite sua altura:");
+    altura = double.Parse(Console.ReadLine());
+Console.WriteLine("Digite seu gênero (M OU F):");
+    genero = char.Parse(Console.ReadLine());    
+Console.WriteLine("Digite seu salário:");
+    salario = double.Parse(Console.ReadLine());
+Console.WriteLine("Olá " + nome + ", você tem " + idade + " anos, sua altura é " + altura + " metros, seu gênero é " + genero + " e seu salário é R$ " + salario);
